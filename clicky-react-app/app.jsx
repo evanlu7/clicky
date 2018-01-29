1c2da925817e4c4afd41919b80954acbaa32c7df
Evan Lu <evanlu7@gmail.com>
2018-01-28 21:25:42 -0800

    redoing it

---
+++ clicky-react-app/app.jsx
@@ -0,0 +1,21 @@
+function Application (){
+	return(
+<div className="top portion">
+	<div className="header">
+<h1>Clicky Game</h1>
+	</div>
+		<div className="images">
+			</div>
+			<div className="player-score">
+				<div className="counter">
+				
+					<div className="counter-score"> 31 </div>
+			</div>
+
+		</div>
+</div>
+		);
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
+
